e7fe9e6740f5cd7ad3f924b94d28c111f4c39e00
Mike Fikes <mike@fikesfarm.com>
2019-06-15 11:05:34 -0400

    Add a project.clj file for deploying

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject cljs-bean "0.4.0"
+  :description "Clojure's bean function for ClojureScript"
+  :url "https://github.com/mfikes/cljs-bean"
+  :scm {:name "git" :url "https://github.com/mfikes/cljs-bean"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojurescript "1.10.520" :scope "provided"]])
