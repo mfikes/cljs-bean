c6e0d0e6807342dc728cc48f3a2d58f99aa9c9ed
Mike Fikes <mike@fikesfarm.com>
2019-06-29 11:16:51 -0400

    nested updates 1

--- test/cljs_bean/core_test.cljs
+++ test/cljs_bean/core_test.cljs
@@ -1157,6 +1157,80 @@
   (is (== 1 ((assoc! (transient (->clj #js [])) 0 1) 0)))
   (is (= :not-found ((assoc! (transient (->clj #js [])) 0 1) :b :not-found))))
 
+(defn expected-js-able? [expected actual]
+  (and (= expected actual) (js-able? actual)))
+
+(defn expected-non-js-able? [expected actual]
+  (and (= expected actual) (not (js-able? actual))))
+
+(deftest nested-update-test
+  (is (expected-js-able? {:a 1, :b 2}
+        (-> #js {:a 1} ->clj (assoc :b 2))))
+  (is (expected-js-able? {:b 2}
+        (-> #js {:a 1} ->clj empty (assoc :b 2))))
+  (is (expected-js-able? {:a 1, :b {:c 3}}
+        (-> #js {:a 1} ->clj (assoc :b (->clj #js {:c 3})))))
+  (is (expected-non-js-able? {:a 1, :b {:c 3}}
+        (-> #js {:a 1} ->clj (assoc :b {:c 3}))))
+  (is (expected-js-able? {:a 1, :b [1]}
+        (-> #js {:a 1} ->clj (assoc :b (->clj #js [1])))))
+  (is (expected-non-js-able? {:a 1, :b [1]}
+        (-> #js {:a 1} ->clj (assoc :b [1]))))
+  (is (expected-js-able? {:a 1}
+        (-> #js {:a 1, :b 2} ->clj (dissoc :b))))
+  (is (expected-js-able? {:a 1, :b 2}
+        (-> #js {:a 1} ->clj (conj [:b 2]))))
+  (is (expected-js-able? {:a 1, :b {:c 3}}
+        (-> #js {:a 1} ->clj (conj [:b (->clj #js {:c 3})]))))
+  (is (expected-non-js-able? {:a 1, :b {:c 3}}
+        (-> #js {:a 1} ->clj (conj [:b {:c 3}]))))
+  (is (expected-js-able? {:a 2}
+        (-> #js {:a 1} ->clj (update :a inc))))
+  (is (expected-js-able? {:a {:b 2}}
+        (-> #js {:a 1} ->clj (update :a (constantly (->clj #js {:b 2}))))))
+  (is (expected-non-js-able? {:a {:b 2}}
+        (-> #js {:a 1} ->clj (update :a (constantly {:b 2})))))
+  (is (expected-js-able? {:a 2}
+        (-> #js {:a 1} ->clj (update-in [:a] inc))))
+
+
+  (is (expected-js-able? [2]
+        (-> #js [1] ->clj (assoc 0 2))))
+  (is (expected-js-able? [1 2]
+        (-> #js [1] ->clj (assoc 1 2))))
+  (is (expected-js-able? [{:c 3}]
+        (-> #js [1] ->clj (assoc 0 (->clj #js {:c 3})))))
+  (is (expected-js-able? [1 {:c 3}]
+        (-> #js [1] ->clj (assoc 1 (->clj #js {:c 3})))))
+  (is (expected-non-js-able? [{:c 3}]
+        (-> #js [1] ->clj (assoc 0 {:c 3}))))
+  (is (expected-js-able? [[1]]
+        (-> #js [1] ->clj (assoc 0 (->clj #js [1])))))
+  (is (expected-non-js-able? [[1]]
+        (-> #js [1] ->clj (assoc 0 [1]))))
+  (is (expected-js-able? [1 [1]]
+        (-> #js [1] ->clj (assoc 1 (->clj #js [1])))))
+  (is (expected-non-js-able? [1 [1]]
+        (-> #js [1] ->clj (assoc 1 [1]))))
+  (is (expected-js-able? [1]
+        (-> #js [1 2] ->clj pop)))
+  (is (expected-js-able? [1 2 3]
+        (-> #js [1] ->clj (conj 2 3))))
+  (is (expected-js-able? [1 {:c 3}]
+        (-> #js [1] ->clj (conj (->clj #js {:c 3})))))
+  (is (expected-non-js-able? [1 {:c 3}]
+        (-> #js [1] ->clj (conj {:c 3}))))
+  (is (expected-js-able? [2]
+        (-> #js [1] ->clj (update 0 inc))))
+  (is (expected-js-able? [1 {:b 2}]
+        (-> #js [1] ->clj (update 1 (constantly (->clj #js {:b 2}))))))
+  (is (expected-non-js-able? [1 {:b 2}]
+        (-> #js [1] ->clj (update 1 (constantly {:b 2})))))
+  (is (expected-non-js-able? [{:b 2}]
+        (-> #js [1] ->clj (update 0 (constantly {:b 2})))))
+  (is (expected-non-js-able? [{:b 2}]
+        (-> #js [1] ->clj (update-in [0] (constantly {:b 2}))))))
+
 (deftest ->clj-test
   (is (nil? (->clj nil)))
   (is (true? (->clj true)))
